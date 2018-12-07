c9326d804531cb770feda11f3861aaddacf254fd
BryanNilsen <bryannilsen@gmail.com>
2018-12-07 00:21:58 -0600

    added style to registration page

--- src/components/authentication/Register.js
+++ src/components/authentication/Register.js
@@ -65,40 +65,40 @@ export default class Register extends Component {
 
   }
 
-    //Handles registration of new user object
-    registerNewUser = newUser => {
-      return APIManager.addEntry("users", newUser)
-    }
+  //Handles registration of new user object
+  registerNewUser = newUser => {
+    return APIManager.addEntry("users", newUser)
+  }
 
 
 
   render() {
     return (
       <React.Fragment>
-        <div  className={this.props.hideLoginForm ? "login_form" : "hide"}>
-          <div>
-            <h2>REGISTER</h2>
+        <div className={this.props.hideLoginForm ? "center basketball_bkg" : "hide"}>
+          <div className="login_form">
+            <h2 className="login_form_title">REGISTER</h2>
 
-            <label className="" htmlFor="registerFName">First Name</label>
-            <input className="" onChange={this.handleFieldChange} type="text" id="registerFName" placeholder="First Name" required="" autoFocus="" />
+            <label className="form_label" htmlFor="registerFName">First Name</label>
+            <input className="form_input" onChange={this.handleFieldChange} type="text" id="registerFName" placeholder="First Name" required="" autoFocus="" />
             <br />
-            <label className="" htmlFor="registerLName">Last Name</label>
-            <input className="" onChange={this.handleFieldChange} type="text" id="registerLName" placeholder="Last Name" required="" />
+            <label className="form_label" htmlFor="registerLName">Last Name</label>
+            <input className="form_input" onChange={this.handleFieldChange} type="text" id="registerLName" placeholder="Last Name" required="" />
             <br />
-            <label className="" htmlFor="registerEmail">Email</label>
-            <input className="" onChange={this.handleFieldChange} type="email" id="registerEmail" placeholder="Email address" required="" autoFocus="" />
+            <label className="form_label" htmlFor="registerEmail">Email</label>
+            <input className="form_input" onChange={this.handleFieldChange} type="email" id="registerEmail" placeholder="Email address" required="" autoFocus="" />
             <br />
-            <label className="" htmlFor="registerNickname">Nickname "AKA"</label>
-            <input className="" onChange={this.handleFieldChange} type="text" id="registerNickname" placeholder="Nickname" required="" />
+            <label className="form_label" htmlFor="registerNickname">Nickname "AKA"</label>
+            <input className="form_input" onChange={this.handleFieldChange} type="text" id="registerNickname" placeholder="Nickname" required="" />
             <br />
-            <label className="" htmlFor="registerHometown">Hometown</label>
-            <input className="" onChange={this.handleFieldChange} type="text" id="registerHometown" placeholder="Hometown" required="" autoFocus="" />
+            <label className="form_label" htmlFor="registerHometown">Hometown</label>
+            <input className="form_input" onChange={this.handleFieldChange} type="text" id="registerHometown" placeholder="Hometown" required="" autoFocus="" />
             <br />
-            <label className="" htmlFor="registerAge">Age</label>
-            <input className="" onChange={this.handleFieldChange} type="text" id="registerAge" placeholder="Age" required="" />
+            <label className="form_label" htmlFor="registerAge">Age</label>
+            <input className="form_input" onChange={this.handleFieldChange} type="text" id="registerAge" placeholder="Age" required="" />
             <br />
-            <label className="" htmlFor="registerHeight">Height</label>
-            <select className="" onChange={this.handleFieldChange} type="text" id="registerHeightFt" required="" autoFocus="" >
+            <label className="form_label" htmlFor="registerHeight">Height</label>
+            <select className="form_select" onChange={this.handleFieldChange} type="text" id="registerHeightFt" required="" autoFocus="" >
               <option value="">Feet</option>
               <option value="3">3</option>
               <option value="4">4</option>
@@ -107,7 +107,7 @@ export default class Register extends Component {
               <option value="7">7</option>
               <option value="8">8</option>
             </select>
-            <select className="" onChange={this.handleFieldChange} type="text" id="registerHeightIn" required="" autoFocus="" >
+            <select className="form_select" onChange={this.handleFieldChange} type="text" id="registerHeightIn" required="" autoFocus="" >
               <option value="">Inches</option>
               <option value="0">0</option>
               <option value="1">1</option>
@@ -125,21 +125,20 @@ export default class Register extends Component {
               <option value="11">11</option>
             </select>
             <br />
-            <label className="" htmlFor="registerPhotoURL">Photo URL</label>
-            <input className="" onChange={this.handleFieldChange} type="text" id="registerPhotoURL" placeholder="Photo URL" required="" />
+            <label className="form_label" htmlFor="registerPhotoURL">Photo URL</label>
+            <input className="form_input" onChange={this.handleFieldChange} type="text" id="registerPhotoURL" placeholder="Photo URL" required="" />
             <br />
-            <label className="" htmlFor="registerPassword">Password</label>
-            <input className="" onChange={this.handleFieldChange} type="password" id="registerPassword" placeholder="Password" required="" autoFocus="" />
+            <label className="form_label" htmlFor="registerPassword">Password</label>
+            <input className="form_input" onChange={this.handleFieldChange} type="password" id="registerPassword" placeholder="Password" required="" autoFocus="" />
             <br />
 
-            <button className="" type="submit" onClick={() => {this.handleRegister()} }>REGISTER</button>
+            <button className="btn_submit"  type="submit" onClick={() => { this.handleRegister() }}>REGISTER</button>
           </div>
 
           <div className="">
             <p>Already Registered?</p>
-            <button onClick={() => this.props.handleChangeForm()}>SIGN IN</button>
+            <button className="btn_submit"  onClick={() => this.props.handleChangeForm()}>SIGN IN</button>
           </div>
-
         </div>
       </React.Fragment>
     )
