5714400824f3d41afd84367beefd04ff438a4180
BryanNilsen <bryannilsen@gmail.com>
2018-12-18 09:18:38 -0600

    Updates

---
+++ precache-manifest.7e5c967d8b0a7cabfb7be7c24341f173.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fc0de3255c0cb0f39ca676c043565369",
+    "url": "/BryanNilsen/Swishlist-React/static/media/basketball_bkg.fc0de325.png"
+  },
+  {
+    "revision": "b05b120774b22f73f2b0",
+    "url": "/BryanNilsen/Swishlist-React/static/js/runtime~main.b05b1207.js"
+  },
+  {
+    "revision": "8218d39bc5210acf5ae8",
+    "url": "/BryanNilsen/Swishlist-React/static/js/main.8218d39b.chunk.js"
+  },
+  {
+    "revision": "30bcf17781621239f763",
+    "url": "/BryanNilsen/Swishlist-React/static/js/1.30bcf177.chunk.js"
+  },
+  {
+    "revision": "8218d39bc5210acf5ae8",
+    "url": "/BryanNilsen/Swishlist-React/static/css/main.991bcf47.chunk.css"
+  },
+  {
+    "revision": "23585243cbfac76d469f6df28240d687",
+    "url": "/BryanNilsen/Swishlist-React/index.html"
+  }
+];
