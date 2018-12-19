923312dbe0b3ce9565fd7e6f4fbcb7e59e947ae1
BryanNilsen <bryannilsen@gmail.com>
2018-12-19 15:54:21 -0600

    additional styling and added asphalt background

--- src/components/Swishlist.js
+++ src/components/Swishlist.js
@@ -8,8 +8,12 @@ export default class Swishlist extends Component {
   render() {
     return (
       <React.Fragment>
+      <div className="background_container">
+      <div className="app_container">
         <Navbar />
         <ApplicationViews handleFieldChange={this.props.handleFieldChange}/>
+      </div>
+      </div>
       </React.Fragment>
     )
   }
